b9601be8f83f76d8e26f25bbab769ef2e9b41255
julian-lenz <67206755+julian-lenz@users.noreply.github.com>
2022-12-11 04:09:07 +0100

    Create Bestellboy.cpp

---
+++ Bestellboy.cpp
@@ -0,0 +1,61 @@
+/*
+**11.Dezember 2022
+**Julian Lenzenweger
+
+*/
+
+#include <iostream>
+#include <string>
+
+typedef class CATEGORY{
+    unsigned int cat;
+} Category
+
+class Beverage{
+public:
+    int Beverage(unsigned int current_count, unsigned int MAX, Category _Category)
+    :   /* TODO */        ;
+    
+    /* ---------- Prop ----------*/
+    unsigned int current_Count();
+    unsigned int required();
+    unsigned short maxQueue();
+    Category cat;
+    
+}
+
+class mailsender(std::string Content, std::string Service){
+    
+}
+
+class inputHandler{
+    
+}
+
+
+using namespace std;
+
+
+int main()
+{
+    /*  Todo Drinks  (Phase one)  */
+    Beverage Hell();
+    Beverage Zwickl();
+    Beverage WeizenH();
+    Beverage WeizenD();
+    Beverage Pils();
+    Beverage Festbier();
+    Beverage AntiBeer();
+    +
+    Beverage Cola();
+    Beverage Fanta();
+    
+
+    return 0;
+}
+
+
+
+/*
+
+*/
